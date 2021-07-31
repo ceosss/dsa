4f6425a7d04ede0031acfb6eef64deef011e0ef1
ceosss <sswarajsamant@gmail.com>
2021-07-31 18:51:24 +0530

    Linked List: delete last item from a linked list

---
+++ linked_list/delete_last_item_from_linked_list.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "linked_list_adt.h"
+
+using namespace std;
+
+int Node::deleteItem()
+{
+    if (!head)
+    {
+        cout << "No item to delete" << endl;
+        return -1;
+    }
+    int deleted;
+    if (!head->next)
+    {
+        struct Node *toDelete = head;
+        deleted = toDelete->data;
+        head = NULL;
+        delete toDelete;
+        return deleted;
+    }
+    else
+    {
+        struct Node *itr = head;
+        struct Node *toDelete;
+        while (itr->next->next)
+            itr = itr->next;
+        toDelete = itr->next;
+        itr->next = NULL;
+        deleted = toDelete->data;
+        delete toDelete;
+        return deleted;
+    }
+}
